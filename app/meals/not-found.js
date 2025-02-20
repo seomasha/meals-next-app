8c79403f7947a30b1ea05caa148f8e3c8c63c26d
seadmasetic <75685855+seomasha@users.noreply.github.com>
2025-02-20 13:41:40 +0100

    Adding not found and meal details page

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function MealNotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>{"Unfortunately, we coulnd't find the requested page."}</p>
+    </main>
+  );
+}

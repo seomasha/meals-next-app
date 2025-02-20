bc0c3b55a24d5fe9b2e39ab9a52a39dc95abb874
seadmasetic <75685855+seomasha@users.noreply.github.com>
2025-02-20 13:24:58 +0100

    Added error handling and not found

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function MealsError({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occurred</h1>
+      <p>Failed to fetch meal data. Try again later.</p>
+    </main>
+  );
+}
